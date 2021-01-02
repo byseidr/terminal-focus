75748fdc39b6143af4ed0e08e971f109d8bc819c
John Anderson Muniz <johnandersonmuniz@gmail.com>
2021-01-02 02:36:23 -0300

    Add project files.

---
+++ terminal-focus/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace terminal_focus
+{
+    class Program
+    {
+        // FindWindow
+        [DllImport("user32.dll", EntryPoint = "FindWindow")]
+        private extern static IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        // SetForegroundWindow
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        private static void sendKeys(string windowName, string keys)
+        {
+            if (SpinWait.SpinUntil(() => FindWindow(null, windowName) != IntPtr.Zero, TimeSpan.FromSeconds(30)))
+            {
+                IntPtr hwnd = FindWindow(null, windowName);
+                SetForegroundWindow(hwnd);
+                SendKeys.SendWait(keys);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = args[0];
+
+            switch (args[0])
+            {
+                case "wt.exe":
+                    p.StartInfo.Arguments = "-p \"" + args[1] + "\"";
+                    p.Start();
+                    sendKeys(args[1], "+{F11}");
+                    break;
+            }
+        }
+    }
+}
